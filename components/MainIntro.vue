237b54dfb19cc1f3d1e51bd87ae3a74d711aed29
Marcelo Rubens Raulino <marcelorubens@Marcelos-MacBook-Pro.local>
2020-02-25 20:32:28 -0300

    first commit

---
+++ components/MainIntro.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div class="container py-3">
+      <p class="font-huge font-bold mb-3">
+        Hi, I'm Marcelo, a <span class="font-featured">designer</span> &
+        <span class="font-featured">web developer</span> from Brazil.
+        <!-- Hi, I'm Marcelo, a designer and
+        web developer from Brazil working
+        in the field for a few years now.
+        Being graduated in graphic design
+        and having studied graphic
+        communication and photography on
+        a study exchange to England, I
+        ended up creating affinity to web
+        design, its processes,
+        experiences and all its
+        possibilities.
+        From early concepts and research,
+        going through wireframing, hi-def
+        mockups and front end code
+        implementation, all the way up to
+        user testing and refinement, I am
+        able to create and analyse
+        pleasant user experiences.
+        I'm a great team player, very
+        curious, enthusiastic and super
+        chilled person seeking for
+        opportunities in the area. -->
+      </p>
+      <h1 class="mb-3">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Obcaecati consectetur quaerat voluptas et soluta id tempora!
+      </h1>
+      <h2>Sit amet pqp.</h2>
+      <h3 class="mb-3">
+        Sit amet.
+      </h3>
+      <p>Dorime ameno latiremo</p>
+      <p>Lorem ipsum dolor sit amet <a href="javascript:void(0)">consectetur adipisicing</a> elit. Omnis vel, vitae voluptatum, inventore adipisci id quibusdam deleniti maxime, animi praesentium aliquid. Nisi, dicta facere reprehenderit iste quis esse tempora illo.</p>
+    </div>
+    <img class="mb-3" src="@/assets/img/stationery.png" alt="Stationery material">
+  </div>
+</template>
+<style lang="scss"></style>
---
+++ pages/index.vue
@@ -0,0 +1,60 @@
+<template>
+  <div>
+    <section class="main-hero d-flex flex-column p-3">
+      <!-- <main-nav /> -->
+      <div class="d-flex align-center justify-center flex-grow">
+        <main-logo />
+      </div>
+    </section>
+    <section class="p-1">
+      <main-intro />
+    </section>
+  </div>
+</template>
+
+<script>
+import MainLogo from '~/components/MainLogo.vue'
+import MainIntro from '~/components/MainIntro.vue'
+// import MainNav from '~/components/MainNav.vue'
+
+export default {
+  components: {
+    MainLogo,
+    MainIntro
+    // MainNav
+  }
+}
+</script>
+
+<style>
+/* .container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+} */
+
+/* .title {
+  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+
+.subtitle {
+  font-weight: 300;
+  font-size: 42px;
+  color: #526488;
+  word-spacing: 5px;
+  padding-bottom: 15px;
+}
+
+.links {
+  padding-top: 15px;
+} */
+</style>
