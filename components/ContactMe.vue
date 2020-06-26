8c82ce9d6b08dba13f26319bf407da8088bb9e77
Marcelo Rubens <marcelorubensraulino@gmail.com>
2020-06-25 21:24:31 -0300

    spacing fixes

--- components/ContactMe.vue
+++ components/ContactMe.vue
@@ -1,13 +1,8 @@
 <template>
   <section id="contactMe" class="d-flex flex-column justify-center">
     <div class="container">
-      <!-- <h1>Contact</h1> -->
-      <div class="">
-        <div>
-          <p>Want to know more about me or my work? Get in touch:</p>
-          <p class="font-huge"><a href="mailto:marcelorubensraulino@gmail.com" target="_blank" class="animate">Send an e-mail</a></p>
-        </div>
-      </div>
+      <p class="mb-1">Want to know more about me or my work? Get in touch:</p>
+      <p class="font-huge"><a href="mailto:marcelorubensraulino@gmail.com" target="_blank" class="animation">Send an e-mail</a></p>
     </div>
   </section>
 </template>
